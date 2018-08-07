7f30a673c3a37ce2de7acecc42a55f4c7100bfd3
DonMahallem <donmahallem@users.noreply.github.com>
2018-08-08 00:36:47 +0200

    web output fixes

--- web/web.cpp
+++ web/web.cpp
@@ -1,9 +1,11 @@
 #include <emscripten/bind.h>
-
+#include "../src/models/sudoku_field.h"
 using namespace emscripten;
 
 float lerp(float a, float b, float t)
 {
+    SudokuField field;
+    field.print();
     return (1 - t) * a + t * b;
 }
 
